69819798cdd3fe35b7eee8bba773efac3d07bd6a
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-22 21:25:29 -0500

    Update about.js

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -30,28 +30,6 @@ export default function About(props) {
           consequat fugiat. Nostrud est ad do reprehenderit excepteur veniam
           incididunt.
         </p>
-        <p>
-          Sit dolore dolor cillum laboris ad pariatur nostrud minim quis eu
-          cillum ullamco. Amet consequat sint ad sint reprehenderit. Anim do
-          adipisicing id fugiat mollit duis occaecat fugiat veniam aliqua
-          aliquip sit velit anim. Nulla adipisicing sint cillum ipsum ad nostrud
-          ipsum. Cupidatat dolor ut dolor labore cupidatat ipsum ullamco
-          cupidatat pariatur deserunt enim consequat irure pariatur. Non
-          incididunt Lorem mollit tempor voluptate qui est voluptate do
-          consequat fugiat. Nostrud est ad do reprehenderit excepteur veniam
-          incididunt.
-        </p>
-        <p>
-          Sit dolore dolor cillum laboris ad pariatur nostrud minim quis eu
-          cillum ullamco. Amet consequat sint ad sint reprehenderit. Anim do
-          adipisicing id fugiat mollit duis occaecat fugiat veniam aliqua
-          aliquip sit velit anim. Nulla adipisicing sint cillum ipsum ad nostrud
-          ipsum. Cupidatat dolor ut dolor labore cupidatat ipsum ullamco
-          cupidatat pariatur deserunt enim consequat irure pariatur. Non
-          incididunt Lorem mollit tempor voluptate qui est voluptate do
-          consequat fugiat. Nostrud est ad do reprehenderit excepteur veniam
-          incididunt.
-        </p>
       </div>
       <FrameContainer>
         <img src={MyImage} alt="Maddie" />
